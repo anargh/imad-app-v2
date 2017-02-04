a0e87748db6bcce78960a4a3a59a774d20470549
nargh <anargh@users.noreply.github.com>
2017-02-04 18:42:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,14 +12,14 @@ function moveRight() {
     var marginLeft = marginLeft + 2;
     img.style.marginLeft = marginLeft + 'px';
 }
-img.onClick = function() {
+img.onclick = function() {
     var interval = setInterval(moveRight, 40);
 };
 
 var btncount=document.getElementById("btn-count"); // Get button
 var count = 0; // Intialize counter value
 
-btncount.onClick = function() {
+btncount.onclick = function() {
     count=count+1;
     var displayCount = document.getElementById("count");
     displayCount.innerHTML = count.toString();
