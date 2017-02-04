90e79dfb2e478fee70eb46933ebcc53a7345c390
nargh <anargh@users.noreply.github.com>
2017-02-04 18:04:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,12 @@
 console.log('Loaded!');
+var element = document.getElementById('main-text');
+element.innerHTML('Click on the image!');
+var img = document.getElementById('madi');
+
+function moveRight() {
+    var marginLeft = marginLeft + 2;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onClick = function() {
+    var interval = setInterval(moveRight, 40);
+}
