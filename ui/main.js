99e5340ce2dd21448ee6a711fd9c0c17e7d4812e
nargh <anargh@users.noreply.github.com>
2017-02-04 18:47:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight() {
     var marginLeft = marginLeft + 2;
-    img.style.marginLeft = marginLeft + 'px';
+    img.style.marginLeft = marginLeft + "px";
 }
 img.onclick = function() {
     var interval = setInterval(moveRight, 40);
