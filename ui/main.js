6f1b5f5f1f29e2e40e0a4e2223a4b24925c283c5
nargh <anargh@users.noreply.github.com>
2017-02-04 18:40:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,21 @@
 console.log('Loaded!');
 
+var element = document.getElementById('main-text');
+
+element.innerHTML='Click on the image!';
+
+
+var img = document.getElementById('madi');
+
+var marginLeft = 0;
+function moveRight() {
+    var marginLeft = marginLeft + 2;
+    img.style.marginLeft = marginLeft + 'px';
+}
+img.onClick = function() {
+    var interval = setInterval(moveRight, 40);
+};
+
 var btncount=document.getElementById("btn-count"); // Get button
 var count = 0; // Intialize counter value
 
