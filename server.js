3b1b86d6f9a36c8e4ac5a32cdaa04e1c3cb179f4
nargh <anargh@users.noreply.github.com>
2017-02-04 16:25:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,59 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articles= {
+    'article-one': {
+        title: "Sample Template || HTML",
+        content: `
+            <h3>Lorum Ipsum</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        `
+    },
+    'article-two': {
+        title: "Sample Template TWO || HTML",
+        content: `
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+        `
+    }
+};
+
+function showContent(data) {
+    var title=data.title;
+    var content=data.content;
+    var htmltemplate = `
+    <html>
+        <title> ${title}
+        <meta name="viewport" content="width=device-width ,initial-scale=1"/>
+        <body>
+            <div class="container">
+                <div>
+                    <a link href="/">Home</a>
+                </div>
+                
+                <hr/>
+                <div>
+                    <h3>Lorum Ipsum</h3>
+                    <p>
+                       ${content}
+                    </p>
+                </div>
+            </div>
+        </body>
+        </html>
+    `
+    return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function(req, res) {
-    res.send('Article one requested!');
+app.get('/:articleName', function(req, res) {
+    var articleName = req.params.articleName;
+    res.send(createTemplate(articleName));
 });
 
 app.get('/template', function(req, res) {
