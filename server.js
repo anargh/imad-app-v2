1cc201d12c53a2270b454c13ac6a088fbcc2ef08
nargh <anargh@users.noreply.github.com>
2017-02-04 18:16:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -64,6 +64,10 @@ app.get('/template', function(req, res) {
     res.sendFile(path.join(__dirname, 'ui', 'template.html'));
 });
 
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
